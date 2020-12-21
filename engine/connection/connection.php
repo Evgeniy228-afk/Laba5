48286bb3d668a3e37f88f44bdb17fe4c005160ee
Evgeniy228-afk <61138496+Evgeniy228-afk@users.noreply.github.com>
2020-12-21 14:00:55 +0300

    commit 1
    com1

---
+++ engine/connection/connection.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$user = 'f0477276_user';
+$password = 'root';
+$database = 'f0477276_db';
+?>
---
+++ engine/page/title.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+	<head>
+        <?require_once 'head.php';?>
+        <title>
+            Евгений Славинский Лабораторная работа №5
+        </title>
+	</head>
+</html>
